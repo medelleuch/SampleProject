eb173fe86debb9bddfb5de2149ad664dd3ca4dd8
user win10 <user win10@DESKTOP-NO9RF1H>
2018-11-24 16:09:49 +0100

    Implement prop file

--- ManagePropFile/src/ReadPropertiesFile.java
+++ ManagePropFile/src/ReadPropertiesFile.java
@@ -1,4 +1,29 @@
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 public class ReadPropertiesFile {
-
+	static Properties prop = new Properties();
+	static InputStream input = null;
+public static void main(String[]args) {
+	try {
+		input = new FileInputStream("config.properties");
+		// load a properties file
+		prop.load(input);
+		// get the property value and print it out
+		System.out.println(prop.getProperty("host"));
+		System.out.println(prop.getProperty("dbuser"));
+		System.out.println(prop.getProperty("dbpassword"));
+	} catch (IOException ex) {
+		ex.printStackTrace();
+	} finally {
+		if (input != null) {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	}
 }
